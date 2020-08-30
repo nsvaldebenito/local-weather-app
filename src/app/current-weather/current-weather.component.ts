2ec71909a8ee82c470c1b34cb62de984c03d5b82
unknown <nsvaldebenito@gmail.com>
2020-08-29 23:56:45 -0400

    template

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  current: ICurrentWeather
+
+  constructor() {
+    this.current = {
+      city: 'Santiago',
+      country: 'Chile',
+      date: new Date(),
+      image: 'assets/img/sunny.png',
+      temperature: 72,
+      description: 'sunny',
+    } as ICurrentWeather
+  }
+
+  ngOnInit(): void {
+  }
+
+}
